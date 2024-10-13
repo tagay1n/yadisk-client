b517740155592a5ea98b0f1b09cacff784177696
tans1q <rkharisov.main@gmail.com>
2024-10-13 23:31:18 +0200

    WIP

--- src/yadisk_client/__init__.py
+++ src/yadisk_client/__init__.py
@@ -4,30 +4,47 @@ import os
 import yadisk
 
 
+class ConflictResolution(enumerate):
+    REPLACE_IF_DIFFERENT = 0
+    ALWAYS_REPLACE = 1
+    SKIP = 2
+
+
 class YaDisk(yadisk.YaDisk):
 
     def __init__(self, token: str):
         super().__init__(token=token)
 
-    def upload_or_replace(self, local_file, remote_dir):
+    def upload_or_replace(self, local_file, remote_dir, conflict_resolution=ConflictResolution.REPLACE_IF_DIFFERENT):
         """
         Uploads a file to the Yandex Disk if it does not exist or has a different hash.
         :param local_file: path to the file to upload
         :param remote_dir: path to the remote folder
+        :param conflict_resolution: strategy to resolve conflicts with existing files
+        if False, the file will be uploaded anyway
         :return: path to the uploaded file
         """
         self.create_folders(remote_dir)
         file_name = os.path.basename(local_file).split('/')[-1]
         remote_path = f"{remote_dir}/{file_name}"
 
-        meta = self.get_meta_or_none(remote_path, fields=["type", "md5"])
-        if meta and meta['type'] == 'dir':
+        cur_meta = self.get_meta_or_none(remote_path, fields=["type", "md5"])
+        if cur_meta and cur_meta['type'] == 'dir':
             raise ValueError(f"Cannot save file because directory with the same name already exists: {remote_path}")
 
-        md5 = calculate_md5(local_file)
-        if (not meta) or meta['md5'] != md5:
-            # Upload the file if it does not exist or has a different hash
+        md5 = None
+        if not cur_meta or conflict_resolution == ConflictResolution.ALWAYS_REPLACE:
             self.upload(local_file, remote_path, overwrite=True)
+            new_meta = self.get_meta_or_none(remote_path, fields=["md5"])
+            md5 = new_meta['md5']
+        elif conflict_resolution == ConflictResolution.REPLACE_IF_DIFFERENT:
+            md5 = calculate_md5(local_file)
+            if cur_meta['md5'] != md5:
+                self.upload(local_file, remote_path, overwrite=True)
+                new_meta = self.get_meta_or_none(remote_path, fields=["md5"])
+                return remote_path, new_meta['md5']
+        elif conflict_resolution == ConflictResolution.SKIP:
+            md5 = cur_meta['md5']
 
         return remote_path, md5
 
